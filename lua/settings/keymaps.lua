69661817906039db16d0b8106353c077dcff0a70
r-okm <r.okm.00113@gmail.com>
2024-09-20 16:35:18 +0900

    "_ (無名レジスタ) keymap 追加

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -9,6 +9,8 @@ vim.keymap.set({ "x" }, "y", "mzy`z")
 -- Visual Mode のインデント調整後に Visual Mode を維持
 vim.keymap.set({ "x" }, "<", "<gv")
 vim.keymap.set({ "x" }, ">", ">gv")
+-- 無名レジスタ
+vim.keymap.set({ "n", "x" }, "<Space>q", '"_')
 
 -- `a"` を `2i"` にマッピング
 -- https://zenn.dev/vim_jp/articles/2024-06-05-vim-middle-class-features#%E5%BC%95%E7%94%A8%E7%AC%A6%E3%81%A7%E5%9B%B2%E3%81%BE%E3%82%8C%E3%81%9F%E7%AE%87%E6%89%80%E5%85%A8%E4%BD%93%E3%82%92%E9%81%B8%E6%8A%9E%E3%81%99%E3%82%8B
