5f694c28e7c1ed27a0c78b6c3dcf3ece6bdcea43
r-okm <r.okm.00113@gmail.com>
2024-10-03 12:00:16 +0900

    cspell 設定ファイルがプロジェクトローカルに存在する場合のみ cspell を有効化

--- lua/plugins/spec/terminal/none-ls.lua
+++ lua/plugins/spec/terminal/none-ls.lua
@@ -11,21 +11,30 @@ return {
     local formatting = nls.builtins.formatting
     local diagnostics = nls.builtins.diagnostics
 
-    local cspell = require("cspell")
+    -- https://github.com/davidmh/cspell.nvim/blob/2c29bf573292c8f5053383d1be4ab908f4ecfc47/lua/cspell/helpers.lua#L7-L13
+    local CSPELL_CONFIG_FILES = {
+      "cspell.json",
+      ".cspell.json",
+      "cSpell.json",
+      ".cSpell.json",
+      ".cspell.config.json",
+    }
+
     local cspell_config = {
       diagnostics_postprocess = function(diagnostic)
         diagnostic.severity = vim.diagnostic.severity["HINT"] -- ERROR, WARN, INFO, HINT
       end,
+      condition = function(utils)
+        return utils.root_has_file(CSPELL_CONFIG_FILES)
+      end,
       config = {
-        find_json = function(_)
-          -- find project local cspell.json or use global cspell.json
-          -- TODO: check file existence
-          return vim.fn.expand(".cspell.json") or vim.fn.expand("~/.config/cspell/cspell.json")
-        end,
+        config_file_preferred_name = ".cspell.json",
+        cspell_config_dirs = { "." },
         decode_json = require("json5").parse,
       },
     }
 
+    local cspell = require("cspell")
     nls.setup({
       -- diagnostics_format = "#{m} (#{s}: #{c})",
       sources = {
