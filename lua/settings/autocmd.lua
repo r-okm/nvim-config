c2432d14bc46d97309122ba7a9a467f968a8627b
r-okm <r.okm.00113@gmail.com>
2024-10-03 10:59:43 +0900

    docker-compose の filetype 判定方法を ftplugin から autocmd に変更

--- lua/settings/autocmd.lua
+++ lua/settings/autocmd.lua
@@ -46,3 +46,11 @@ vim.api.nvim_create_autocmd("VimLeavePre", {
     return true
   end,
 })
+
+-- yaml.docker-compose filetype を設定する
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "docker-compose.yaml", "docker-compose.yml", "compose.yaml", "compose.yml" },
+  callback = function()
+    vim.bo.filetype = "yaml.docker-compose"
+  end,
+})
