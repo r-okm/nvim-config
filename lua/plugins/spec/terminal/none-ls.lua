df2d7a4bb3f449c3fd18d27daa71c74c120fd74f
r-okm <r.okm.00113@gmail.com>
2024-10-03 11:18:02 +0900

    feat: none-ls に cspell 用の設定を追加
    以下の変更を行った:
    - none-ls に cspell.nvim を追加
    - none-ls に lua-json5 を追加
    - cspell の設定を追加し、診断の重大度を HINT に設定
    - cspell の設定でプロジェクトローカルの cspell.json を検索
    - mason-lock.json に cspell を追加

--- lua/plugins/spec/terminal/none-ls.lua
+++ lua/plugins/spec/terminal/none-ls.lua
@@ -1,7 +1,9 @@
 return {
   "nvimtools/none-ls.nvim",
   dependencies = {
-    "jay-babu/mason-null-ls.nvim",
+    { "jay-babu/mason-null-ls.nvim" },
+    { "davidmh/cspell.nvim" },
+    { "Joakker/lua-json5", name = "json5", build = "./install.sh" },
   },
   event = { "BufReadPost" },
   config = function()
@@ -9,9 +11,26 @@ return {
     local formatting = nls.builtins.formatting
     local diagnostics = nls.builtins.diagnostics
 
+    local cspell = require("cspell")
+    local cspell_config = {
+      diagnostics_postprocess = function(diagnostic)
+        diagnostic.severity = vim.diagnostic.severity["HINT"] -- ERROR, WARN, INFO, HINT
+      end,
+      config = {
+        find_json = function(_)
+          -- find project local cspell.json or use global cspell.json
+          -- TODO: check file existence
+          return vim.fn.expand(".cspell.json") or vim.fn.expand("~/.config/cspell/cspell.json")
+        end,
+        decode_json = require("json5").parse,
+      },
+    }
+
     nls.setup({
       -- diagnostics_format = "#{m} (#{s}: #{c})",
       sources = {
+        cspell.code_actions.with(cspell_config),
+        cspell.diagnostics.with(cspell_config),
         formatting.prettierd.with({
           prefer_local = "node_modules/.bin",
           env = {
