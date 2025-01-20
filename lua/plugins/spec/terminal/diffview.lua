23ad3aac580bd86f14b33bfc0c86929d547a78b2
r-okm <r.okm.00113@gmail.com>
2025-01-20 15:18:16 +0900

    feat(diffview.nvim): add mergetool integration

--- lua/plugins/spec/terminal/diffview.lua
+++ lua/plugins/spec/terminal/diffview.lua
@@ -8,18 +8,31 @@ return {
     { "zh", mode = { "x" } },
     { "zl", mode = { "n" } },
   },
-  cmd = { "DiffviewFileHistory" },
+  cmd = { "DiffviewFileHistory", "DiffviewOpen" },
   config = function()
     vim.keymap.set({ "n" }, "zh", ":<C-u>DiffviewFileHistory %<CR>", { silent = true })
     vim.keymap.set({ "x" }, "zh", ":'<,'>DiffviewFileHistory<CR>", { silent = true })
     vim.keymap.set({ "n" }, "zl", ":<C-u>DiffviewFileHistory<CR>", { silent = true })
 
+    local actions = require("diffview.config").actions
     require("diffview").setup({
+      view = {
+        merge_tool = {
+          layout = "diff3_mixed",
+        },
+      },
       keymaps = {
         view = {
           { "n", "q", ":<C-u>tabc<CR>" },
+          ["g."] = actions.next_conflict,
+          ["g,"] = actions.prev_conflict,
+          ["g1"] = actions.conflict_choose("ours"),
+          ["g2"] = actions.conflict_choose("theirs"),
         },
         file_history_panel = {
+          { "n", "q", ":<C-u>tabc<CR>" },
+        },
+        file_panel = {
           { "n", "q", ":<C-u>tabc<CR>" },
         },
       },
