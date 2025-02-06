4531acd3261957e1fcb1e571f55866a9560a3056
r-okm <65703649+r-okm@users.noreply.github.com>
2025-02-06 13:35:27 +0900

    oil: add `oil-ssh` keymap

--- lua/plugins/spec/terminal/oil.lua
+++ lua/plugins/spec/terminal/oil.lua
@@ -5,31 +5,30 @@ return {
   },
   branch = "master",
   lazy = false,
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {
-    keymaps = {
-      ["g?"] = { "actions.show_help", mode = "n" },
-      ["<CR>"] = "actions.select",
-      ["<C-l>"] = "actions.preview",
-      ["q"] = { "actions.close", mode = "n" },
-      ["<C-h>"] = { "actions.parent", mode = "n" },
-      ["-"] = { "actions.open_cwd", mode = "n" },
-    },
-    use_default_keymaps = false,
-    view_options = {
-      show_hidden = true,
-    },
-  },
-  keys = {
-    {
-      "<space>e",
-      function()
-        require("oil").open_float(nil, {
-          preview = { vertical = true },
-        })
-      end,
-      mode = { "n" },
-    },
-  },
+  config = function()
+    local oil = require("oil")
+    oil.setup({
+      keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-l>"] = "actions.preview",
+        ["q"] = { "actions.close", mode = "n" },
+        ["<C-h>"] = { "actions.parent", mode = "n" },
+        ["-"] = { "actions.open_cwd", mode = "n" },
+      },
+      use_default_keymaps = false,
+      view_options = {
+        show_hidden = true,
+      },
+    })
+    vim.keymap.set({ "n" }, "<space>e", function()
+      oil.open_float(nil, {
+        preview = { vertical = true },
+      })
+    end)
+    vim.keymap.set({ "n" }, "<space>E", function()
+      oil.open_float()
+    end)
+    vim.keymap.set("ca", "os", "e oil-ssh://")
+  end,
 }
