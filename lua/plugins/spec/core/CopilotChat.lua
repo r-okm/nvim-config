d7ac4b0c7446755f971ee4488cf78b5bb61cf9bf
r-okm <65703649+r-okm@users.noreply.github.com>
2025-02-21 22:18:59 +0900

    CopilotChat: remove additional prompts and configure chat UI
    - Remove appended additional prompts from commit configuration.
    - Configure chat UI headers by setting `question_header`, `answer_header`, and
      `error_header` for improved UI consistency.

--- lua/plugins/spec/core/CopilotChat.lua
+++ lua/plugins/spec/core/CopilotChat.lua
@@ -14,39 +14,27 @@ return {
     local chat = require("CopilotChat")
     local select = require("CopilotChat.select")
     local actions = require("CopilotChat.actions")
-    local default_prompts = require("CopilotChat.config.prompts")
+    local prompts = require("CopilotChat.config.prompts")
     local telescope = require("CopilotChat.integrations.telescope")
 
-    local additional_prompt = [[ Your answer must be JAPANESE.
-また、回答は以下の規約に従う。
-* 日本語で回答する
-* 敬体を使用せず、常体を使用する
-* 英語の回答は日本語に翻訳して回答する
-* 日本語文中の半角英数字は前後に半角スペースを一つ入れる
-]]
-    local prompts = {
-      Japanese = "Translate the provided English sentence into Japanese.",
-      English = "Translate the provided Japanese sentence into English.",
-    }
-    for prompt_title, config in pairs(default_prompts) do
-      prompts[prompt_title] = config
-      if config.prompt ~= nil then
-        prompts[prompt_title].prompt = config.prompt .. additional_prompt
-      end
-    end
-
-    prompts.Review.callback = function(_, _)
-      -- highlight を無効にするため、何もしない
-      -- https://github.com/CopilotC-Nvim/CopilotChat.nvim/issues/362#issuecomment-2241158016
-    end
+    prompts.Japanese = "Translate the provided English sentence into Japanese."
+    prompts.English = "Translate the provided Japanese sentence into English."
     prompts.CommitEditmsg = {
       prompt = "Write commit message for the change. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit. Proper nouns should be enclosed in backquotes. Use bullet points for descrptions. Summay must begin with a simple topic name followed by a colon.",
       selection = select.buffer,
     }
 
+    prompts.Review.callback = function(_, _)
+      -- highlight を無効にするため、何もしない
+      -- https://github.com/CopilotC-Nvim/CopilotChat.nvim/issues/362#issuecomment-2241158016
+    end
+
     chat.setup({
       model = "o3-mini",
       prompts = prompts,
+      question_header = "#  User ",
+      answer_header = "#  Copilot ",
+      error_header = "#  Error ",
       mappings = {
         complete = {
           detail = "Use @<Tab> or /<Tab> for options.",
