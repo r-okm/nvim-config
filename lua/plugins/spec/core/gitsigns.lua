e99f45e2a9a69dbdad5d7694e8d1509010d5e2fe
r-okm <65703649+r-okm@users.noreply.github.com>
2025-03-07 17:03:54 +0900

    gitsigns: add keymaps aboud hunk actions
    * Replace single command-mode shortcut with multiple normal-mode mappings
    * Add shortcuts for common git operations:
      - `zn` to toggle line blame
      - `zm`/`zM` to preview and reset hunks
      - `z.`/`z,` for navigating between hunks

--- lua/plugins/spec/core/gitsigns.lua
+++ lua/plugins/spec/core/gitsigns.lua
@@ -7,6 +7,14 @@ return {
         delay = 500,
       },
     })
-    vim.keymap.set("ca", "gst", "Gitsigns toggle_current_line_blame")
+
+    local keymap = function(mode, lhs, rhs)
+      vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true })
+    end
+    keymap("n", "zn", ":<C-u>Gitsigns toggle_current_line_blame<CR>")
+    keymap("n", "zm", ":<C-u>Gitsigns preview_hunk_inline<CR>")
+    keymap("n", "zM", ":<C-u>Gitsigns reset_hunk<CR>")
+    keymap("n", "z.", ":<C-u>Gitsigns next_hunk<CR>")
+    keymap("n", "z,", ":<C-u>Gitsigns prev_hunk<CR>")
   end,
 }
