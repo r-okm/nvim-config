ac1ca5cb3dd0cd7ef58a4c857ff0336562857358
r-okm <65703649+r-okm@users.noreply.github.com>
2025-03-17 16:48:41 +0900

    telescope: specify branch and update to latest commit
    - Added explicit `branch = "master"` to `telescope.nvim` plugin spec
    - Updated `telescope.nvim` to newer commit in lazy-lock.json

--- lua/plugins/spec/core/telescope.lua
+++ lua/plugins/spec/core/telescope.lua
@@ -2,6 +2,7 @@ local getVisualSelection = require("r-okm.util").getVisualSelection
 
 return {
   "nvim-telescope/telescope.nvim",
+  branch = "master",
   dependencies = {
     { "nvim-lua/plenary.nvim", branch = "master" },
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
