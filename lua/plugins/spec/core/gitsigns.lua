280c52f6227a8a20ca1abe272abc467cb3af3a0a
r-okm <65703649+r-okm@users.noreply.github.com>
2025-04-18 19:12:48 +0900

    gitsigns: use preview_hunk instead of inline
    - Replace `preview_hunk_inline` with `preview_hunk` for `zm` mapping.
    - Simplifies keybinding by dropping inline preview suffix.

--- lua/plugins/spec/core/gitsigns.lua
+++ lua/plugins/spec/core/gitsigns.lua
@@ -13,7 +13,7 @@ return {
       vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true })
     end
     keymap("n", "zn", ":<C-u>Gitsigns toggle_current_line_blame<CR>")
-    keymap("n", "zm", ":<C-u>Gitsigns preview_hunk_inline<CR>")
+    keymap("n", "zm", ":<C-u>Gitsigns preview_hunk<CR>")
     keymap("n", "zM", ":<C-u>Gitsigns reset_hunk<CR>")
     keymap("n", "z.", ":<C-u>Gitsigns next_hunk<CR>")
     keymap("n", "z,", ":<C-u>Gitsigns prev_hunk<CR>")
