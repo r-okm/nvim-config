be0391ec5ce50bbd7d5e50261046d66805fadff3
r-okm <65703649+r-okm@users.noreply.github.com>
2025-04-27 09:23:37 +0900

    lsp: move `lua_ls` setup into `lazydev` settings
    - Deleted standalone `lua_ls` config in favor of `lazydev`'s library settings
    - Added `lazy` plugins directory to workspace library in `lazydev` config

--- lua/plugins/spec/core/lazydev.lua
+++ lua/plugins/spec/core/lazydev.lua
@@ -1,9 +1,11 @@
+---@type LazyPluginSpec
 return {
   "folke/lazydev.nvim",
   ft = "lua",
   opts = {
     library = {
       { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      { path = vim.fn.stdpath("data") .. "/lazy" },
     },
   },
 }
