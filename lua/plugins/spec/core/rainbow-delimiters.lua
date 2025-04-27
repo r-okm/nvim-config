4154b72e59d985a88f76a2dc434b840de442396d
r-okm <65703649+r-okm@users.noreply.github.com>
2025-04-27 17:41:09 +0900

    rainbow-delimiters: add rainbow parentheses with treesitter support
    - integrate `rainbow-delimiters.nvim` for nested delimiter highlighting
    - configure custom highlight groups for improved visibility

---
+++ lua/plugins/spec/core/rainbow-delimiters.lua
@@ -0,0 +1,18 @@
+---@type LazyPluginSpec
+return {
+  "https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
+  event = { "BufReadPost" },
+  dependencies = {
+    { "nvim-treesitter/nvim-treesitter" },
+  },
+  main = "rainbow-delimiters.setup",
+  ---@type rainbow_delimiters.config
+  opts = {
+    highlight = {
+      "RainbowDelimiterYellow",
+      "RainbowDelimiterOrange",
+      "RainbowDelimiterViolet",
+      "RainbowDelimiterCyan",
+    },
+  },
+}
