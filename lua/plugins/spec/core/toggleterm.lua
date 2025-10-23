c9ee5969ab9abb9bc4e25afeeaa0747a8f04c93a
r-okm <65703649+r-okm@users.noreply.github.com>
2025-10-23 17:22:46 +0900

    toggleterm: disable terminal in neovim, keep only lazygit
    - Comment out `<C-k><C-n>` keybinding
    - Comment out `toggleterm.setup()` configuration
    - Comment out `TermOpen` autocmd for escape key mapping
    - Keep only `lazygit` toggle functionality with `zg` key
    - Aims to use tmux window instead

--- lua/plugins/spec/core/toggleterm.lua
+++ lua/plugins/spec/core/toggleterm.lua
@@ -5,7 +5,7 @@ return {
   "akinsho/toggleterm.nvim",
   keys = {
     { "zg", mode = { "n" } },
-    { "<C-k><C-n>", mode = { "n" } },
+    -- { "<C-k><C-n>", mode = { "n" } },
   },
   config = function()
     local Terminal = require("toggleterm.terminal").Terminal
@@ -19,21 +19,21 @@ return {
     end
     util.keymap({ "n" }, "zg", "<cmd>lua LazygitToggle()<CR>")
 
-    require("toggleterm").setup({
-      open_mapping = { "<C-k><C-n>" },
-      hide_numbers = true,
-      start_in_insert = true,
-      persist_size = true,
-      persist_mode = false,
-      direction = "tab",
-      close_on_exit = true,
-      auto_scroll = true,
-    })
-    vim.api.nvim_create_autocmd("TermOpen", {
-      pattern = "term://*",
-      callback = function(opts)
-        util.keymap("t", [[<C-\>]], [[<C-\><C-n>]], { buffer = opts.buf })
-      end,
-    })
+    -- require("toggleterm").setup({
+    --   open_mapping = { "<C-k><C-n>" },
+    --   hide_numbers = true,
+    --   start_in_insert = true,
+    --   persist_size = true,
+    --   persist_mode = false,
+    --   direction = "tab",
+    --   close_on_exit = true,
+    --   auto_scroll = true,
+    -- })
+    -- vim.api.nvim_create_autocmd("TermOpen", {
+    --   pattern = "term://*",
+    --   callback = function(opts)
+    --     util.keymap("t", [[<C-\>]], [[<C-\><C-n>]], { buffer = opts.buf })
+    --   end,
+    -- })
   end,
 }
