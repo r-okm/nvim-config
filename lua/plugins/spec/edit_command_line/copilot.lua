1a5f9e4ef4547d18bf4a4972f2507db738d0e0a1
r-okm <65703649+r-okm@users.noreply.github.com>
2025-10-31 17:38:46 +0900

    copilot: remove cmp popup integration
    - remove event handlers for `menu_opened` and `menu_closed`
    - no longer hide copilot suggestions when cmp popup is visible

--- lua/plugins/spec/edit_command_line/copilot.lua
+++ lua/plugins/spec/edit_command_line/copilot.lua
@@ -21,13 +21,5 @@ return {
         gitcommit = true,
       },
     })
-
-    -- disalbe when popup is visible
-    require("cmp").event:on("menu_opened", function()
-      vim.b.copilot_suggestion_hidden = true
-    end)
-    require("cmp").event:on("menu_closed", function()
-      vim.b.copilot_suggestion_hidden = false
-    end)
   end,
 }
