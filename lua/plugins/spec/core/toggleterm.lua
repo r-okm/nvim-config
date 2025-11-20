fbbb35f9ef17f441401e081a072bdbc7b16312f5
r-okm <65703649+r-okm@users.noreply.github.com>
2025-11-20 16:36:53 +0900

    toggleterm: add copilot cli and enable setup
    - Add `copilot` CLI terminal with `zy` key binding
    - Enable `toggleterm` setup with size configuration
    - Add terminal mode window navigation keymaps
    - Uncomment `<C-k><C-n>` mapping for default terminal

--- lua/plugins/spec/core/toggleterm.lua
+++ lua/plugins/spec/core/toggleterm.lua
@@ -5,10 +5,12 @@ return {
   "akinsho/toggleterm.nvim",
   keys = {
     { "zg", mode = { "n" } },
-    -- { "<C-k><C-n>", mode = { "n" } },
+    { "zy", mode = { "n" } },
+    { "<C-k><C-n>", mode = { "n" } },
   },
   config = function()
     local Terminal = require("toggleterm.terminal").Terminal
+
     local lazygit = Terminal:new({
       cmd = "lazygit",
       direction = "tab",
@@ -19,21 +21,42 @@ return {
     end
     util.keymap({ "n" }, "zg", "<cmd>lua LazygitToggle()<CR>")
 
-    -- require("toggleterm").setup({
-    --   open_mapping = { "<C-k><C-n>" },
-    --   hide_numbers = true,
-    --   start_in_insert = true,
-    --   persist_size = true,
-    --   persist_mode = false,
-    --   direction = "tab",
-    --   close_on_exit = true,
-    --   auto_scroll = true,
-    -- })
-    -- vim.api.nvim_create_autocmd("TermOpen", {
-    --   pattern = "term://*",
-    --   callback = function(opts)
-    --     util.keymap("t", [[<C-\>]], [[<C-\><C-n>]], { buffer = opts.buf })
-    --   end,
-    -- })
+    local copilot_cli = Terminal:new({
+      cmd = "copilot",
+      direction = "vertical",
+      hidden = true,
+    })
+    function CopilotCliToggle()
+      copilot_cli:toggle()
+    end
+    util.keymap({ "n" }, "zy", "<cmd>lua CopilotCliToggle()<CR>")
+
+    require("toggleterm").setup({
+      open_mapping = { "<C-k><C-n>" },
+      hide_numbers = true,
+      start_in_insert = true,
+      persist_size = true,
+      persist_mode = false,
+      direction = "tab",
+      close_on_exit = true,
+      auto_scroll = true,
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        else
+          return 20
+        end
+      end,
+    })
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "term://*",
+      callback = function(opts)
+        util.keymap("t", [[<C-\>]], [[<C-\><C-n>]], { buffer = opts.buf })
+        util.keymap("t", "<C-w><C-w>", [[<Cmd>wincmd w<CR>]], { buffer = opts.buf })
+        util.keymap("t", "<C-w>w", [[<Cmd>wincmd w<CR>]], { buffer = opts.buf })
+      end,
+    })
   end,
 }
