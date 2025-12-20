2939c69a7df5cdc15fa67c15a2643a02a1d352f1
r-okm <65703649+r-okm@users.noreply.github.com>
2025-12-20 12:23:58 +0900

    vim-matchup: add vim-matchup plugin and config
    - Add `vim-matchup` spec in `edit_command_line/vim-matchup.lua`
    - Enable treesitter-related options under `init` for `vim-matchup`

---
+++ lua/plugins/spec/edit_command_line/vim-matchup.lua
@@ -0,0 +1,10 @@
+---@type LazyPluginSpec
+return {
+  "andymass/vim-matchup",
+  init = function()
+    vim.g.matchup_treesitter_stopline = 500
+    vim.g.matchup_treesitter_enable_quotes = true
+    vim.g.matchup_treesitter_disable_virtual_text = true
+    vim.g.matchup_treesitter_include_match_words = true
+  end,
+}
