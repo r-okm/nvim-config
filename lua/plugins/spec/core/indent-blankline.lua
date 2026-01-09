760b51c7e12f93a099009cba8da565c4c30a5f0b
r-okm <65703649+r-okm@users.noreply.github.com>
2026-01-09 17:54:42 +0900

    indent-blankline: remove rainbowline dependency
    - Remove `TheGLander/indent-rainbowline.nvim` from dependencies

--- lua/plugins/spec/core/indent-blankline.lua
+++ lua/plugins/spec/core/indent-blankline.lua
@@ -1,15 +1,11 @@
 ---@type LazyPluginSpec
 return {
   "lukas-reineke/indent-blankline.nvim",
-  dependencies = {
-    "TheGLander/indent-rainbowline.nvim",
-  },
   main = "ibl",
   event = { "BufReadPost" },
-  config = function()
-    local opts = require("indent-rainbowline").make_opts({
-      scope = { enabled = false },
-    })
-    require("ibl").setup(opts)
-  end,
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = {
+    scope = { enabled = false },
+  },
 }
