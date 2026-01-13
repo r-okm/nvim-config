85b988ccfa3e9dc4f5b92ddeb2b83fb223cbcf91
r-okm <65703649+r-okm@users.noreply.github.com>
2026-01-13 11:06:44 +0900

    bufdelete: add silent option to keymap

--- lua/plugins/spec/core/bufdelete.lua
+++ lua/plugins/spec/core/bufdelete.lua
@@ -2,7 +2,7 @@
 return {
   "famiu/bufdelete.nvim",
   keys = {
-    { "<leader>w", ":<C-u>Bdelete<CR>", mode = { "n" } },
+    { "<leader>w", ":<C-u>Bdelete<CR>", mode = { "n" }, silent = true },
   },
   cmd = { "Bdelete", "Bwipeout" },
 }
