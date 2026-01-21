07f26a945815cc122dd3b3c91d335c6c1be2f7bf
r-okm <65703649+r-okm@users.noreply.github.com>
2026-01-21 12:07:34 +0900

    tmux-edit: add dedicated neovim config entry point

---
+++ tmux_edit.lua
@@ -0,0 +1,21 @@
+require("config.options")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  spec = {
+    { import = "plugins.spec.edit_command_line" },
+    { import = "plugins.spec.theme" },
+  },
+})
+
+require("config.keymaps")
+require("config.usercommand")
+require("config.autocmd")
+
+-- Set autowriteall option locally
+vim.opt_local.autowriteall = true
+-- Set filetype to markdown
+vim.bo.filetype = "markdown"
+-- Start in insert mode
+vim.cmd("startinsert")
