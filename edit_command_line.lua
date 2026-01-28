269f0beecb359100257a0cd9b273ef66dfc0c547
r-okm <65703649+r-okm@users.noreply.github.com>
2026-01-29 08:49:53 +0900

    editor: add cmdline abbrev for saving and quitting
    - Add `qq` cmdline abbreviation for executing `wqa` in
      `edit_command_line.lua` and `tmux_edit.lua`
    - Remove local `autowriteall` option setting in both files

--- edit_command_line.lua
+++ edit_command_line.lua
@@ -13,7 +13,9 @@ require("config.keymaps")
 require("config.usercommand")
 require("config.autocmd")
 
--- Set autowriteall option locally
-vim.opt_local.autowriteall = true
+local util = require("r-okm.util")
+
+-- Set abbreviation for quitting all and saving
+util.keymap("ca", "qq", "execute 'wqa'")
 -- Set filetype to zsh
 vim.bo.filetype = "zsh"
--- tmux_edit.lua
+++ tmux_edit.lua
@@ -13,8 +13,10 @@ require("config.keymaps")
 require("config.usercommand")
 require("config.autocmd")
 
--- Set autowriteall option locally
-vim.opt_local.autowriteall = true
+local util = require("r-okm.util")
+
+-- Set abbreviation for quitting all and saving
+util.keymap("ca", "qq", "execute 'wqa'")
 -- Set filetype to markdown
 vim.bo.filetype = "markdown"
 -- Start in insert mode
