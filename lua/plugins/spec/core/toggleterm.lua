69be2e96a59f6990d2e380f08cc6298836ceb727
r-okm <65703649+r-okm@users.noreply.github.com>
2026-02-13 16:32:46 +0900

    toggleterm: refactor keymaps and terminal autocmd
    - Move keymaps from config to keys table with explicit commands
    - Add `<C-k><C-m>` keymap for vertical terminal toggle
    - Move TermOpen autocmd from config to init function
    - Add terminal mode keymaps for `<C-k><C-n>` and `<C-k><C-m>` in autocmd
    - Remove copilot_cli terminal configuration
    - Change vertical terminal width from 40% to 50% of columns
    - Add `cmd` table to lazy-load plugin

--- lua/plugins/spec/core/toggleterm.lua
+++ lua/plugins/spec/core/toggleterm.lua
@@ -4,10 +4,23 @@ local util = require("r-okm.util")
 return {
   "https://github.com/akinsho/toggleterm.nvim",
   keys = {
-    { "zg", mode = { "n" } },
-    { "zy", mode = { "n" } },
-    { "<C-k><C-n>", mode = { "n" } },
+    { "zg", "<cmd>lua LazygitToggle()<CR>", mode = { "n" }, noremap = true, silent = true },
+    { "<C-k><C-n>", "<cmd>ToggleTerm direction=tab<CR>", mode = { "n" }, noremap = true, silent = true },
+    { "<C-k><C-m>", "<cmd>ToggleTerm direction=vertical<CR>", mode = { "n" }, noremap = true, silent = true },
   },
+  cmd = { "ToggleTerm" },
+  init = function()
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "term://*",
+      callback = function(opts)
+        util.keymap("t", [[<C-\>]], [[<C-\><C-n>]], { buffer = opts.buf })
+        util.keymap("t", "<C-w><C-w>", [[<Cmd>wincmd w<CR>]], { buffer = opts.buf })
+        util.keymap("t", "<C-w>w", [[<Cmd>wincmd w<CR>]], { buffer = opts.buf })
+        util.keymap("t", "<C-k><C-n>", [[<Esc><Cmd>ToggleTerm<CR>]], { buffer = opts.buf })
+        util.keymap("t", "<C-k><C-m>", [[<Esc><Cmd>ToggleTerm<CR>]], { buffer = opts.buf })
+      end,
+    })
+  end,
   config = function()
     local Terminal = require("toggleterm.terminal").Terminal
 
@@ -19,20 +32,8 @@ return {
     function LazygitToggle()
       lazygit:toggle()
     end
-    util.keymap({ "n" }, "zg", "<cmd>lua LazygitToggle()<CR>")
-
-    local copilot_cli = Terminal:new({
-      cmd = "copilot",
-      direction = "vertical",
-      hidden = true,
-    })
-    function CopilotCliToggle()
-      copilot_cli:toggle()
-    end
-    util.keymap({ "n" }, "zy", "<cmd>lua CopilotCliToggle()<CR>")
 
     require("toggleterm").setup({
-      open_mapping = { "<C-k><C-n>" },
       hide_numbers = true,
       start_in_insert = true,
       persist_size = true,
@@ -44,18 +45,10 @@ return {
         if term.direction == "horizontal" then
           return 15
         elseif term.direction == "vertical" then
-          return vim.o.columns * 0.4
+          return vim.o.columns * 0.5
         else
           return 20
         end
-      end,
-    })
-    vim.api.nvim_create_autocmd("TermOpen", {
-      pattern = "term://*",
-      callback = function(opts)
-        util.keymap("t", [[<C-\>]], [[<C-\><C-n>]], { buffer = opts.buf })
-        util.keymap("t", "<C-w><C-w>", [[<Cmd>wincmd w<CR>]], { buffer = opts.buf })
-        util.keymap("t", "<C-w>w", [[<Cmd>wincmd w<CR>]], { buffer = opts.buf })
       end,
     })
   end,
