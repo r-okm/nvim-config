35c3867002e47e3901f94aac2659e623c7582ce1
r-okm <65703649+r-okm@users.noreply.github.com>
2026-04-06 14:12:13 +0900

    colorizer: migrate from norcalli to catgoose fork
    - Replace norcalli/nvim-colorizer.lua with catgoose/nvim-colorizer.lua
    - Adopt new structured options format (parsers.names, parsers.rgb)

--- lua/plugins/spec/core/nvim-colorizer.lua
+++ lua/plugins/spec/core/nvim-colorizer.lua
@@ -1,13 +1,19 @@
 ---@type LazyPluginSpec
 return {
-  "https://github.com/norcalli/nvim-colorizer.lua",
+  "https://github.com/catgoose/nvim-colorizer.lua",
   event = { "BufReadPost" },
   config = function()
-    local filetype_opts = { "*", "!toggleterm", css = { names = true } }
-    local default_opts = {
-      names = false,
-      rgb_fn = true,
-    }
-    require("colorizer").setup(filetype_opts, default_opts)
+    require("colorizer").setup({
+      filetypes = {
+        "*",
+        css = { parsers = { names = { enable = true } } },
+      },
+      options = {
+        parsers = {
+          names = { enable = false },
+          rgb = { enable = true },
+        },
+      },
+    })
   end,
 }
