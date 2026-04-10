ef04ccfea7da792db93c385b2d916eb930e07295
r-okm <65703649+r-okm@users.noreply.github.com>
2026-04-10 11:55:53 +0900

    vimdoc-ja: use lazy events to manage tags-ja lifecycle
    - Replace build-time git restore with lazy Pre/Post event
      hooks to properly manage the helptags regeneration cycle
    - Pre-sync: git restore doc/tags-ja to clear diff that blocks
      lazy.nvim git operations
    - Post-sync: regenerate helptags with Neovim to ensure correct
      tags are always used, regardless of plugin update status
    - Upstream ships tags-ja with !_TAG_FILE_ENCODING line that
      Neovim no longer outputs, causing persistent git dirty state

--- lua/plugins/spec/core/vimdoc-ja.lua
+++ lua/plugins/spec/core/vimdoc-ja.lua
@@ -2,5 +2,19 @@
 return {
   "https://github.com/vim-jp/vimdoc-ja",
   branch = "master",
-  build = "git restore doc/tags-ja",
+  init = function(plugin)
+    local docs = plugin.dir .. "/doc"
+    vim.api.nvim_create_autocmd("User", {
+      pattern = { "LazySyncPre", "LazyUpdatePre", "LazyInstallPre" },
+      callback = function()
+        vim.fn.system({ "git", "-C", plugin.dir, "restore", "doc/tags-ja" })
+      end,
+    })
+    vim.api.nvim_create_autocmd("User", {
+      pattern = { "LazySync", "LazyUpdate", "LazyInstall" },
+      callback = function()
+        vim.cmd.helptags(docs)
+      end,
+    })
+  end,
 }
